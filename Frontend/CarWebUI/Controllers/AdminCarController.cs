99e96f2e8356228b5dd829de50adda916cbe8521
kosebaris1 <kosebaris464@gmail.com>
2025-07-24 23:38:34 +0300

    admin layout integrated

---
+++ Frontend/CarWebUI/Controllers/AdminCarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarWebUI.Controllers
+{
+    public class AdminCarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
